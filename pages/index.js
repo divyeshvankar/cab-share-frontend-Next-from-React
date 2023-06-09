b514d46ed23bb1ddaf413bd0fa72f82447d7a8ad
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-06-09 10:54:56 +0530

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import Head from 'next/head'
 import Homepage from './Homepage'
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Cabs 4u</title>
-        <meta name="description" content="Cabs4u gives you affordable and cheapest ride in Vadodara and many more cities." />
+        <title>cabs in vadodara 24/7 | Cabs 4u </title>
+        <meta name="description" content="Cabs4u gives you cheapest and affordable  ride in Vadodara and many more cities. 24/7 service available. " />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content="Cabs4u,cabs4u,cabs 4 u, cabs for you , cabs in vadodara, taxi in vadodara, taxi service in vadodara, cabs in vadodara to mumbai,cabs in vadodara airport, night cabs in vadodara,outstation cab in vadodara,cheapest cab in vadodara,online cab in vadodara "></meta>
+        <meta name="keywords" content="Cabs4u,cabs4u,cabs 4 u, cabs for you , taxi in vadodara, taxi in vadodara, taxi service in vadodara, taxi in vadodara to mumbai,taxi in vadodara airport, night taxi in vadodara,outstation taxi in vadodara,cheapest taxi in vadodara,online taxi in vadodara "></meta>
+        <link rel="canonical" href="https://www.cabs4u.in/" />
         {/* <link rel="icon" href="/favicon.svg" /> */}
         <link rel="icon" href="fevicon.svg" />
         {/* <link rel="shortcut icon" href="final.ico" type="image/x-icon" /> */}
         
-        <Helmet>
-              
-        </Helmet>
+        
         
 
 
