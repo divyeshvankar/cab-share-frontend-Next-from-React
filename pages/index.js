82d9e28c76b9c6fa6844cb70344719a2b22bf9bc
Divyesh Vankar <divyeshvankar.iitg@gmail.com>
2023-06-11 23:27:27 +0530

    added google tags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Homepage from './Homepage'
+
 // import { Helmet } from "react-helmet";
 
 
@@ -13,6 +14,22 @@ export default function Home() {
         <meta name="keywords" content="Cabs4u,cabs4u,cabs 4 u, cabs for you , cabs in vadodara, taxi in vadodara, taxi service in vadodara, cabs in vadodara to mumbai,cabs in vadodara airport, night cabs in vadodara,outstation cab in vadodara,cheapest cab in vadodara,online cab in vadodara "></meta>
         <meta name="keywords" content="Cabs4u,cabs4u,cabs 4 u, cabs for you , taxi in vadodara, taxi in vadodara, taxi service in vadodara, taxi in vadodara to mumbai,taxi in vadodara airport, night taxi in vadodara,outstation taxi in vadodara,cheapest taxi in vadodara,online taxi in vadodara "></meta>
         <link rel="canonical" href="https://www.cabs4u.in/" />
+        {/* <!-- Google tag (gtag.js) --> */}
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10981971260"></script>
+        <script strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-10981971260');
+        gtag('event', 'conversion', {'send_to': 'AW-10981971260/vxuqCPiRlqoYELyqzvQo'});
+      `}
+    </script>
+
+
+
+
         {/* <link rel="icon" href="/favicon.svg" /> */}
         <link rel="icon" href="fevicon.svg" />
         {/* <link rel="shortcut icon" href="final.ico" type="image/x-icon" /> */}
