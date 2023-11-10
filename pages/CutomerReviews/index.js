567cf43a69fb145358bab8b75e77bb6465728051
divyeshvankar <divyeshvankar.iitg@gmail.com>
2023-11-10 21:21:09 +0530

    added review design

--- pages/CutomerReviews/index.js
+++ pages/CutomerReviews/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
+import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 
 const CustomerReviews = () => {
   const [reviews, setReviews] = useState([
     {
       name: 'Haresh',
       title: 'Great Service',
-      rating: 5,
+      rating: 3,
       comment: 'I love this cabs service! It is very affordable.',
     },
     {
-      name: 'Dev',
+      name: 'Raj',
       title: 'Excellent Service',
       rating: 4,
       comment: 'The customer service was great, and they answered all my questions.',
@@ -23,7 +24,7 @@ const CustomerReviews = () => {
     {
       name: 'Divyesh',
       title: 'Satisfied',
-      rating: 4,
+      rating: 5,
       comment: 'I had a good experience with this service. It did meet my expectations.',
     },
   ]);
@@ -44,10 +45,10 @@ const CustomerReviews = () => {
     setReviews([...reviews, newReview]);
     // Reset the form fields
     setNewReview({
-        name: '',
-        title: '',
-        rating: 5,
-        comment: '',
+      name: '',
+      title: '',
+      rating: 5,
+      comment: '',
     });
   };
 
@@ -67,94 +68,96 @@ const CustomerReviews = () => {
   };
 
   return (
-        <>
+    <>
       <div id="cars" className="rent-mainContainer">
-      <div className="rent-container">
-        <div className="rent-title">
-          <p>Top 4 Customer</p>
-          <h1>Reviews</h1>
+        <div className="rent-container">
+          <div className="rent-title">
+            <p>Top 4 Customer</p>
+            <h1>Reviews</h1>
+          </div>
+          <ul className="rent-cards">
+            {reviews.map((review, index) => (
+              <li key={index} className="rent-card rent-description">
+                <h1>{review.name}</h1>
+                <h2>{review.title}</h2>
+                <h1>
+                  <div className="rating">
+                  {Array.from({ length: 5 }, (_, i) => {
+                    const isHalfStar = i + 0.5 === review.rating;
+                    const isFilled = i + 1 <= review.rating;
+                    
+                    if (isHalfStar) {
+                      return <FaStarHalfAlt key={i} color="gold" />;
+                    } else if (isFilled) {
+                      return <FaStar key={i} color="gold" />;
+                    } else {
+                      return <FaRegStar key={i} color="gold" />;
+                    }
+                  })}
+                </div>
+                    </h1>
+                <p>{review.comment}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-        <ul className="rent-cards">
-          {reviews.map((review, index) => (
-            <li key={index} className="rent-card rent-description">
-              <h1>Name: {review.name}</h1>
-              <h2>{review.title}</h2>
-              <p>Rating: {review.rating}</p>
-              <p>{review.comment}</p>
-            </li>
-          ))}
-        </ul>
-        
       </div>
-      
-    </div>
-    <div id="form" className="form-mainContainer">
-    <div className="form-container"> 
-    <div className="form-title">
-                <p>Give your </p>
-                <h1>Review</h1>
-              </div>
-              <div className="form-content">
+      <div id="form" className="form-mainContainer">
+        <div className="form-container">
+          <div className="form-title">
+            <p>Give your </p>
+            <h1>Review</h1>
+          </div>
+          <div className="form-content">
+            <div className="form-form">
+              <form>
+                <div className="form-input">
+                  <input
+                    type="text"
+                    placeholder="Your Name"
+                    id="name"
+                    name="name"
+                    value={newReview.name}
+                    onChange={handleInputChange}
+                  />
 
-              
-                <div className="form-form">
-      <form>
-         
-          <div className="form-input">
-          <input
-                        type="text"
-                        placeholder='Your Name'
-                        id="name"
-                        name="name"
-                        value={newReview.name}
-                        onChange={handleInputChange}
-                      />
-           
-          
-          
-            
-            <input
-              type="text"
-              placeholder='Title'
-              id="title"
-              name="title"
-              value={newReview.title}
-              onChange={handleInputChange}
-              />
-          
-          
-            
-            <input
-              type="number"
-              id="rating"
-              placeholder='Rating'
-              name="rating"
-              value={newReview.rating}
-              onChange={handleInputChange}
-              />
-         
-          
-           
-            <textarea
-              id="comment"
-              placeholder='Comment'
-              name="comment"
-              value={newReview.comment}
-              onChange={handleInputChange}
-              />
-          
-          
-              </div>
-          <div className="form-submit">
-          <button type="button" onClick={addReview}>
-            Add Review
-          </button>
-        </div>
-        </form>
-        </div></div>
+                  <input
+                    type="text"
+                    placeholder="Title of review"
+                    id="title"
+                    name="title"
+                    value={newReview.title}
+                    onChange={handleInputChange}
+                  />
+
+                  <input
+                    type="number"
+                    id="rating"
+                    placeholder="Rating"
+                    name="rating"
+                    value={newReview.rating}
+                    onChange={handleInputChange}
+                  />
+
+                  <textarea
+                    id="comment"
+                    placeholder="Comment"
+                    name="comment"
+                    value={newReview.comment}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="form-submit">
+                  <button type="button" onClick={addReview}>
+                    Add Review
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
-  </>
+    </>
   );
 };
 
